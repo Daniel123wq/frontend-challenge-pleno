5b20576f6d95e4d6119deb9a64ba7f6829a8af42
Daniel <danielolhaso@msn.com>
2021-11-20 22:45:53 -0300

    Feat: Create nav style, with empty role;

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Site/>
+  </div>
+</template>
+
+<script>
+import Site from './components/Site.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Site
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: $font-stack;
+  }
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark pad">
+    <div class="container-fluid mt-4">
+      <a class="navbar-brand" href="#">
+        <img
+          class="logo"
+          src="~@/assets/logo.png"
+          alt=""
+          width="117"
+          height="20"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Sobre</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Preços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Desenvolvedores</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="input-group mb-0">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+              />
+              <button
+                class="btn btn-outline"
+                type="button"
+              >
+                <b-icon font-scale="1" icon="search" variant="white"></b-icon>
+              </button>
+            </div>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ajuda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contato</a>
+            </li>
+            <button type="button" class="btn btn-outline-light rounded-pill">Começar</button>
+          </ul>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: {},
+};
+</script>
